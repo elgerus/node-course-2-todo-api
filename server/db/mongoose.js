dade208bd61565b14699a62b991c6c28204aee0f
elgerus <elgerus@gmail.com>
2017-09-05 19:46:10 +0200

    Added heroku dependencies

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,5 +5,7 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://<dbuser>:<dbpassword>@ds125774.mlab.com:25774/mongoose-for-nodejs-course'
 };
-mongoose.connect( process.env.MONGODB_URI || db.mlab);
+var connectURI = process.env.MONGODB_URI || db.localhost;
+console.log(connectURI);
+mongoose.connect(connectURI);
 module.export = {mongoose};
