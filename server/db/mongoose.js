bbb897d93f4596e2f4b240801b78df35378fa8b5
elgerus <elgerus@gmail.com>
2017-10-02 22:43:48 +0200

    Setup separated config test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://<dbuser>:<dbpassword>@ds125774.mlab.com:25774/mongoose-for-nodejs-course'
-};
-var connectURI = process.env.MONGODB_URI || db.localhost;
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://<dbuser>:<dbpassword>@ds125774.mlab.com:25774/mongoose-for-nodejs-course'
+// };
+var connectURI = process.env.MONGODB_URI;
 console.log(connectURI);
 mongoose.connect(connectURI);
 module.export = {mongoose};
